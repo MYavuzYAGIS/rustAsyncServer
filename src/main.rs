b6f3fd1051307c335730fcd9bedaedacd6746f94
yavuzyagis <m.yavuzyagis@gmail.com>
2023-02-22 13:50:28 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::f32::consts::E;
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+const ECHO_SERVER_ADDRESS: &str = "localhost:1234";
+
 fn main() {
-    println!("Hello, world!");
+    //connection
+    println!("connecting to {}", ECHO_SERVER_ADDRESS);
 }
