f849fcf68d96477062c0f19fbe165c2771120173
yavuzyagis <m.yavuzyagis@gmail.com>
2023-02-22 14:03:55 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,18 @@ fn main() {
     println!("connecting to {}", ECHO_SERVER_ADDRESS);
 
     if let Ok(mut stream) = TcpStream::connect(ECHO_SERVER_ADDRESS) {
+        //connected
         println!(
             "connected to {}:{}",
             stream.local_addr().unwrap().ip(),
             stream.local_addr().unwrap().port()
         );
+
+        //write to socket
+        let message: &str = "Hello World!";
+        let _ = stream.write(message.as_bytes());
+        let _ = stream.flush();
+        println!("Sent: {}", message);
     } else {
         println!("Failed to connect to {}", ECHO_SERVER_ADDRESS);
     }
